669a20ee48e970ad5a2d4268970e99dc6200b393
Jaobacca <joaoarthurmh@gmail.com>
2022-01-19 01:23:29 -0300

    Criando class livro

--- src/br/com/dio/PriomeiroPrograma.java
+++ src/br/com/dio/PriomeiroPrograma.java
@@ -10,11 +10,44 @@ public class PriomeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+        Livro livro = new Livro("Antologia Poética Vinicius de moraes", 400);
 
+        System.out.println(livro);
 
-/*
-        int a = 5;
-        int b = 3;
-        System.out.println("hello word!" + (a+b)) ;*/
     }
+}
+
+    class Livro
+    {
+        private String nome;
+        private Integer numPag;
+
+        public Livro(String nome, Integer numPag) {
+            this.nome = nome;
+            this.numPag = numPag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPag() {
+            return numPag;
+        }
+
+        public void setNumPag(Integer numPag) {
+            this.numPag = numPag;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPag=" + numPag +
+                    '}';
+        }
 }
